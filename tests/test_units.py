0ededbcc5c79cfdfc762c5516a072d5802f4134f
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2026-04-10 17:21:32 +0100

    Create tests for PygmentsLineState

--- tests/test_units.py
+++ tests/test_units.py
@@ -116,3 +116,97 @@ def test_split_parsed_codeblock(children, expected_text, expected_markup: list):
 
     assert text == expected_text
     assert markup == expected_markup
+
+
+PYGMENTS_LINE_STATE_ITERATIONS_CASES = (
+    (r'<span>text</span>', (('<span>', 'text', r'</span>'),)),
+    (r'<span>text 82&*(d//span   ddd</span>', (('<span>', 'text 82&*(d//span   ddd', r'</span>'),)),
+    (r'<span><b>text</b></span>', (('<span><b>', 'text', '</b></span>'),)),
+    (r'<span><b><em>text</em></b></span>', (('<span><b><em>', 'text', '</em></b></span>'),)),
+    (r'<span class="a b c ddaj">text</span>', (('<span class="a b c ddaj">', 'text', r'</span>'),)),
+    (r'<span>text</span><span>value</span>',
+     (('<span>', 'text', r'</span>'), ('<span>', 'value', r'</span>'),)),
+    (r'<span class="a">text</span><span><b><em>value</em></b></span>',
+     (('<span class="a">', 'text', r'</span>'), ('<span><b><em>', 'value', r'</em></b></span>'),)),
+)
+
+
+@pytest.mark.parametrize('source_str,expected',PYGMENTS_LINE_STATE_ITERATIONS_CASES)
+def test_pygments_line_state_dunder(source_str, expected):
+    state = spc.PygmentsLineState(source_str)
+
+    assert state.html_span == expected[0][0]
+    assert state.text == expected[0][1]
+    assert state.html_close == expected[0][2]
+
+    for expected_html, expected_text, expected_close in expected[1:]:
+        state.next()
+        assert state.html_span == expected_html
+        assert state.text == expected_text
+        assert state.html_close == expected_close
+
+    with pytest.raises(StopIteration):
+        next(state)
+
+
+@pytest.mark.parametrize('source_str,expected',PYGMENTS_LINE_STATE_ITERATIONS_CASES)
+def test_pygments_line_state_iter(source_str, expected):
+    state = spc.PygmentsLineState(source_str)
+
+    assert state.html_span == expected[0][0]
+    assert state.text == expected[0][1]
+    assert state.html_close == expected[0][2]
+
+    i = -1
+    for i, (span, text, close) in enumerate(state.iter()):
+        expected_html, expected_text, expected_close = expected[i+1]
+        assert span == expected_html
+        assert state.html_span == expected_html
+
+        assert text == expected_text
+        assert state.text == expected_text
+
+        assert close == expected_close
+        assert state.html_close == expected_close
+
+    assert i+2 == len(expected)
+
+
+@pytest.mark.parametrize('source_str,expected',PYGMENTS_LINE_STATE_ITERATIONS_CASES)
+def test_pygments_line_state_restore_span(source_str, expected):
+    state = spc.PygmentsLineState(source_str)
+    assert state.restore_span() == ''.join(expected[0])
+
+
+@pytest.mark.parametrize(
+    'source_str,text,expected',
+    (
+        (r'<span>text</span>', 'new text', r'<span>new text</span>'),
+        (r'<span>text 82&*(d//span   ddd</span>', '777', r'<span>777</span>'),
+        (r'<span><b>text</b></span>', '<em>emphasis</em>', r'<span><b><em>emphasis</em></b></span>'),
+        (r'<span><b><em>text</em></b></span>', '', r'<span><b><em></em></b></span>'),
+    )
+)
+def test_pygments_line_state_create_span(source_str, text, expected):
+    state = spc.PygmentsLineState(source_str)
+    assert state.create_span(text) == expected
+
+
+@pytest.mark.parametrize(
+    'source_str,n,expected',
+    (
+        (r'<span>text</span>', 1, r'ext'),
+        (r'<span>text 82&*(d//span   ddd</span>', 5, r'82&*(d//span   ddd'),
+        (r'<span><b>text</b></span>', 2, r'xt'),
+        (r'<span><b><em>text</em></b></span>', 4, r''),
+    )
+)
+def test_pygments_line_state_cut(source_str, n, expected):
+    state = spc.PygmentsLineState(source_str)
+    close = state.html_close
+
+    assert state.cut(n) is None
+    assert state.html_span == ''
+    assert state.text == expected
+    assert state.html_close == close
+
